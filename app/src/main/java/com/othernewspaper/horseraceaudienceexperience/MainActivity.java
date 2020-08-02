6260190d4a73a31b55191bcf0044e26576c5f27f
morten <mortenhh@gmail.com>
2020-08-03 01:01:11 +0200

    third

--- app/src/main/java/com/othernewspaper/horseraceaudienceexperience/MainActivity.java
+++ app/src/main/java/com/othernewspaper/horseraceaudienceexperience/MainActivity.java
@@ -44,7 +44,7 @@ import java.io.ByteArrayOutputStream;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
+//
 import butterknife.ButterKnife;
 import de.hdodenhof.circleimageview.CircleImageView;
 
